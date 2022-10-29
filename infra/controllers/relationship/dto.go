d3e92e2f6be767efb9be8cb35966433a69f70c9c
Wilker Alves <wilkeralves97@gmail.com>
2022-10-29 12:24:21 -0300

    implementando endpoint para relacionamento

--- infra/controllers/relationship/dto.go
+++ infra/controllers/relationship/dto.go
@@ -1,6 +1,6 @@
 package relationship
 
 type CreateRelationship struct {
-	Parent   int
-	Children int
+	Parent   int `json:"parent" binding:"required,gte=1"`
+	Children int `json:"children" binding:"required,gte=1"`
 }
